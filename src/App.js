8c5847514d17dbe769b6e071e84f005881372bce
IrsaAbid123 <irsabid123899@gmail.com>
2024-03-09 13:25:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import {useState} from 'react'
 import logo from './logo.svg';
+import Card from './component/Card';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  function increment() {
+    setCounter(counter + 1)
+  }
+  function decrement() {
+    setCounter(counter - 1)
+  }
+
+
+   const data = {
+   name: 'Irsa',
+   class: 13,
+   section: "A"
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <h1>counter: {counter}</h1>
+   <button onClick={increment} className='bg-blue-300 m-1 p-1 rounded border-black border-2'>increment</button>
+   <button onClick={decrement} className='bg-blue-300 m-1 p-1 rounded border-black border-2'>decrement</button>
+   <Card data= {data}/>
+   <Card data= {data}/>
+    </>
   );
 }
 
